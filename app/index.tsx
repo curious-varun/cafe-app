01bbc51227a631330cec73ef4b7a0988b9219dcd
Varun <devvarunbhardwaj@gmail.com>
2025-06-27 12:41:06 +0530

    added tailwind

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import "./globals.css"
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="">
+      <View className="h-[72] bg-green-800 p-20">
+        <Text className="bg-red-100" >
+          Welcome to Nativewind!
+        </Text>
+      </View>
+    </View >
+  );
+}
